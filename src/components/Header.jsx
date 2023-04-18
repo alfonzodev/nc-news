077570c29c4a41429be8086ba734b500c4f781e2
Jose Alfonzo <alfonzo.dev@gmail.com>
2023-04-18 10:56:52 +0100

    Add Header and Loading Banner components

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { FaRegNewspaper } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="navbar">
+      <Link to={"/"}>
+        <div className="logo-container">
+          <span id="logo-icon">
+            <FaRegNewspaper />
+          </span>
+          <span id="logo-text">NC News</span>
+        </div>
+      </Link>
+      <div className="nav-btn-container">
+        <Link>
+          <button className="nav-btn">Login</button>
+        </Link>
+        <Link>
+          <button className="nav-btn">Register</button>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/LoadingBanner.jsx
@@ -0,0 +1,10 @@
+const LoadingBanner = () => {
+  return (
+    <div className="loading-banner">
+      <p>Loading...</p>
+      <p>Please wait while we fetch your articles.</p>
+    </div>
+  );
+};
+
+export default LoadingBanner;
