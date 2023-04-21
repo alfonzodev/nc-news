9f6dba85901e84011c04a7ae00cb72319eed3ca3
Jose Alfonzo <alfonzo.dev@gmail.com>
2023-04-21 15:52:20 +0100

    solve 11-delete-comments

--- src/api.js
+++ src/api.js
@@ -6,46 +6,65 @@ const api = axios.create({
   baseURL: BASE_URL,
 });
 
-
 export const fetchArticles = (topic, sort_by, order) => {
-    return api.get('/articles', {params: {topic, sort_by, order}}).then(({data}) => {
-        return data;
-    })
-}
+  return api
+    .get("/articles", { params: { topic, sort_by, order } })
+    .then(({ data }) => {
+      return data;
+    });
+};
 export const fetchArticleById = (article_id) => {
-    return api.get(`/articles/${article_id}`).then(({data}) => {
-        return data;
-    })
-}
+  return api.get(`/articles/${article_id}`).then(({ data }) => {
+    return data;
+  });
+};
 
 export const fetchArticleComments = (article_id) => {
-    return api.get(`/articles/${article_id}/comments`).then(({data}) => {
-        return data;
-    })
-}
+  return api.get(`/articles/${article_id}/comments`).then(({ data }) => {
+    return data;
+  });
+};
 
 export const incrementArticleVotes = (article_id, inc_votes) => {
-  return api.patch(`/articles/${article_id}`, {inc_votes}).then(({data}) => {
+  return api
+    .patch(`/articles/${article_id}`, { inc_votes })
+    .then(({ data }) => {
       return data;
-  })
-}
+    });
+};
 
 export const postComment = (article_id, username, body) => {
-return api.post(`/articles/${article_id}/comments`, {username, body}).then(({data}) => {
-    return data;
-})
-}
+  return api
+    .post(`/articles/${article_id}/comments`, { username, body })
+    .then(({ data }) => {
+      return data;
+    });
+};
 
 export const loginUser = (email, password) => {
-    return api.post('/users/login', {email, password}, {withCredentials: true }).then(({data}) => {
-        return data;
-    }).catch((err) => {
-        return Promise.reject(err);
+  return api
+    .post("/users/login", { email, password }, { withCredentials: true })
+    .then(({ data }) => {
+      return data;
     })
-}
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+};
 
 export const getTopics = () => {
-    return api.get('/topics').then(({data}) => {
-        return data;
+  return api.get("/topics").then(({ data }) => {
+    return data;
+  });
+};
+
+export const deleteCommentById = (commentId) => {
+  return api
+    .delete(`/comments/${commentId}`)
+    .then(() => {
+      return "success";
     })
-}
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+};
--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,6 +1,34 @@
-import { timestampToDate } from "../utils/utils";
+import { useContext, useState } from "react";
+import { MdOutlineDeleteOutline } from "react-icons/md";
+import { toast } from "react-toastify";
 
-const CommentCard = ({ comment }) => {
+import { UserContext } from "../context/User";
+import { timestampToDate } from "../utils/utils";
+import { deleteCommentById } from "../api";
+
+const CommentCard = ({ comment, setComments }) => {
+  const [clicked, setClicked] = useState(false);
+  const { user } = useContext(UserContext);
+
+  const handleClick = (e) => {
+    setClicked(true);
+    if (user !== comment.author) {
+      toast.error("You are not the author of this comment.");
+    } else {
+      deleteCommentById(comment.comment_id).then(() => {
+        toast.success('Comment deleted!')
+        setComments((comments) => {
+          return comments.filter(
+            (com) => com.comment_id !== comment.comment_id
+          );
+        });
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+    setClicked(false);
+  };
+
   return (
     <li className="comment-card">
       <div className="comment-header">
@@ -14,6 +42,13 @@ const CommentCard = ({ comment }) => {
         <div className="comment-body">
           <p>{comment.body}</p>
         </div>
+        <button
+          className="btn-comment-del"
+          disabled={clicked}
+          onClick={handleClick}
+        >
+          <MdOutlineDeleteOutline />
+        </button>
       </div>
     </li>
   );
