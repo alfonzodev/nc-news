7c34fc5924c2536b04fc91252e9c0693652fa666
Jose Alfonzo <alfonzo.dev@gmail.com>
2023-04-23 00:04:26 +0100

    improve code formatting in api.js

--- src/api.js
+++ src/api.js
@@ -4,68 +4,42 @@ const BASE_URL = "https://top-tier-articles.onrender.com/api/";
 
 const api = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true
+  withCredentials: true,
 });
 
 export const fetchArticles = (topic, sort_by, order) => {
   return api
     .get("/articles", { params: { topic, sort_by, order } })
-    .then(({ data }) => {
-      return data;
-    });
+    .then(({ data }) => data);
 };
 export const fetchArticleById = (article_id) => {
-  return api.get(`/articles/${article_id}`).then(({ data }) => {
-    return data;
-  });
+  return api.get(`/articles/${article_id}`).then(({ data }) => data);
 };
 
 export const fetchArticleComments = (article_id) => {
-  return api.get(`/articles/${article_id}/comments`).then(({ data }) => {
-    return data;
-  });
+  return api.get(`/articles/${article_id}/comments`).then(({ data }) => data);
 };
 
 export const incrementArticleVotes = (article_id, inc_votes) => {
   return api
     .patch(`/articles/${article_id}`, { inc_votes })
-    .then(({ data }) => {
-      return data;
-    });
+    .then(({ data }) => data);
 };
 
 export const postComment = (article_id, username, body) => {
   return api
     .post(`/articles/${article_id}/comments`, { username, body })
-    .then(({ data }) => {
-      return data;
-    });
+    .then(({ data }) => data);
 };
 
 export const loginUser = (email, password) => {
-  return api
-    .post("/users/login", { email, password })
-    .then(({ data }) => {
-      return data;
-    })
-    .catch((err) => {
-      return Promise.reject(err);
-    });
+  return api.post("/users/login", { email, password }).then(({ data }) => data);
 };
 
 export const getTopics = () => {
-  return api.get("/topics").then(({ data }) => {
-    return data;
-  });
+  return api.get("/topics").then(({ data }) => data);
 };
 
 export const deleteCommentById = (commentId) => {
-  return api
-    .delete(`/comments/${commentId}`)
-    .then(() => {
-      return "success";
-    })
-    .catch((err) => {
-      return Promise.reject(err);
-    });
+  return api.delete(`/comments/${commentId}`);
 };
