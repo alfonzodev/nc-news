6bdbe920e68e346e345e3a990f28cc4fc9bea365
Jose Alfonzo <alfonzo.dev@gmail.com>
2023-04-23 00:01:39 +0100

    add frontend 404 error handling for inexistent url

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Home from "./pages/Home.jsx";
 import Articles from "./pages/Articles.jsx";
 import SingleArticle from "./pages/SingleArticle.jsx";
 import Login from "./pages/Login.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 import Header from "./components/Header.jsx";
 
@@ -24,6 +25,7 @@ const App = () => {
             <Route path="/articles" element={<Articles />} />
             <Route path="/articles/:article_id" element={<SingleArticle />} />
             <Route path="/login" element={<Login />} />
+            <Route path="*" element={<ErrorPage error={404}/>}/>
           </Routes>
           <ToastContainer />
         </UserProvider>
