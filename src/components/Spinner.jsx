a2a73a2cb9d1bcf22cdb22dcbd0b93282c1e2868
Jose Alfonzo <alfonzo.dev@gmail.com>
2023-04-23 17:01:25 +0100

    add Spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import { ClipLoader } from "react-spinners"
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+        <ClipLoader color="#1b9db4" size={150}/> 
+    </div>
+  )
+}
+
+export default Spinner
