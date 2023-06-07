88fe135092cecf0b08796dfcd1819b11add71e34
Jose Alfonzo <alfonzo.dev@gmail.com>
2023-06-07 18:36:02 +0100

    add edit dropdown menu to article cards

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,23 +1,98 @@
 import { useNavigate } from "react-router-dom";
 import { timestampToDate } from "../utils/utils";
+import { useState } from "react";
+import { BiDotsVerticalRounded } from "react-icons/bi";
+import { deleteArticleById } from "../api";
+import { toast } from "react-toastify";
+import { ClipLoader } from "react-spinners";
 
-const ArticleCard = ({ article }) => {
+const ArticleCard = ({ article, editable, setMyArticles }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
   let navigate = useNavigate();
+
   const handleClick = () => {
     navigate(`/articles/${article.article_id}`);
   };
 
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const deleteArticle = (id) => {
+    handleMenuToggle();
+    setIsDeleting(true);
+    deleteArticleById(id)
+      .then(() => {
+        setIsDeleting(false);
+        setMyArticles((articles) =>
+          articles.filter((article) => article.article_id !== id)
+        );
+      })
+      .catch((err) => {
+        setIsDeleting(false);
+        toast.error("Article could not be deleted.");
+      });
+  };
+
   return (
     <li className="article-card" data-article-id={article.article_id}>
-        <div className="title-container">
-            <h1>{article.title}</h1>
-        </div>
+      {editable && (
+        <>
+          <div
+            className="menu-icon"
+            style={{
+              position: "absolute",
+              top: "20px",
+              right: "10px",
+              cursor: "pointer",
+              fontSize: "20px",
+            }}
+            onClick={handleMenuToggle}
+          >
+            <BiDotsVerticalRounded />
+          </div>
+
+          {isMenuOpen && (
+            <div
+              className="menu-options"
+              style={{
+                position: "absolute",
+                top: "40px",
+                right: "20px",
+                width: "min(90%, 200px)",
+                background: "white",
+                boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.3)",
+              }}
+            >
+              <div
+                onClick={() => {
+                  deleteArticle(article.article_id);
+                }}
+                style={{
+                  padding: "8px",
+                  cursor: "pointer",
+                  textAlign: "center",
+                  color: "#f8333c",
+                }}
+              >
+                Delete Article
+              </div>
+            </div>
+          )}
+        </>
+      )}
+      {isDeleting && <ClipLoader />}
+      <div className="title-container">
+        <h1>{article.title}</h1>
+      </div>
       <div className="img-container">
         <img src={article.article_img_url} alt={article.topic} />
       </div>
       <div className="article-card-info">
-      <span className="info-span">By: {article.author}</span>
-      <span className="info-span">{timestampToDate(article.created_at)}</span>
+        <span className="info-span">By: {article.author}</span>
+        <span className="info-span">{timestampToDate(article.created_at)}</span>
       </div>
       <button
         onClick={() => {
--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -4,10 +4,9 @@ import { useSearchParams } from "react-router-dom";
 import ArticleCard from "./ArticleCard";
 import LoadingBanner from "./LoadingBanner";
 
-
 import { fetchArticles } from "../api";
 
-const ArticlesList = ({setError}) => {
+const ArticlesList = ({ setError }) => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -19,7 +18,7 @@ const ArticlesList = ({setError}) => {
 
   useEffect(() => {
     setIsLoading(true);
-    fetchArticles({topic, sort_by, order})
+    fetchArticles({ topic, sort_by, order })
       .then((data) => {
         setArticles(data.articles);
         setIsLoading(false);
@@ -29,12 +28,18 @@ const ArticlesList = ({setError}) => {
       });
   }, [topic, sort_by, order, searchParams, setError]);
 
-  if(isLoading) return <LoadingBanner typeOfData={"articles"} />
-  
+  if (isLoading) return <LoadingBanner typeOfData={"articles"} />;
+
   return (
     <ul className="articles-list">
       {articles.map((article) => {
-        return <ArticleCard key={article.article_id} article={article} />
+        return (
+          <ArticleCard
+            key={article.article_id}
+            article={article}
+            editable={false}
+          />
+        );
       })}
     </ul>
   );
