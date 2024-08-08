c40a7d7156aac1eeef4ea67b921689170e43f2d4
Gregorio Alfonzo <alfonzo.dev@gmail.com>
2024-08-08 14:21:25 +0100

    add styling to single article page component

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -5,7 +5,6 @@ import { UserContext } from "../context/User";
 import { postComment } from "../api";
 import errorMessages from "../utils/errorMessages";
 
-
 const CommentForm = ({ articleId, setComments }) => {
   const { user } = useContext(UserContext);
 
@@ -18,7 +17,7 @@ const CommentForm = ({ articleId, setComments }) => {
       setCommentBody("");
       return toast.warning("Invalid: empty comment");
     }
-    if(!user) return toast.error(errorMessages[401]);
+    if (!user) return toast.error(errorMessages[401]);
 
     setIsSending(true);
 
@@ -37,23 +36,38 @@ const CommentForm = ({ articleId, setComments }) => {
     setCommentBody(e.target.value);
   };
   return (
-    <>
-      <form className="comment-form" onSubmit={handleSubmit}>
+    <div className="w-full border-2 border-transparent focus-within:border-primary shadow-md shadow-gray-400 rounded-md p-2">
+      {/* Show User Avatar and Name if Logged in */}
+      {user && (
+        <div className="flex justify-start items-center gap-2 mb-2">
+          <img
+            src={user.avatar_url}
+            alt="user profile"
+            className="w-8 border border-gray-400 rounded-full"
+          />
+          <span className="text-sm font-medium">{user.name}</span>
+        </div>
+      )}
+
+      <form className="" onSubmit={handleSubmit}>
         <textarea
-          className="comment-form-body"
+          className="w-full focus:outline-none p-1 resize-none overflow-y-auto min-h-20"
           value={commentBody}
           onChange={handleChange}
           placeholder="Share your thoughts..."
           required={true}
         ></textarea>
-        <input
-          className="btn comment-form-btn"
-          type="submit"
-          value="comment"
-          disabled={isSending}
-        />
+        <hr />
+        <div className="flex justify-end mt-2">
+          <input
+            className="py-2 px-4 border-transparent bg-blue-600 hover:bg-blue-800 text-sm text-white font-medium rounded-sm cursor-pointer"
+            type="submit"
+            value="Comment"
+            disabled={isSending}
+          />
+        </div>
       </form>
-    </>
+    </div>
   );
 };
 
--- src/pages/SingleArticle.jsx
+++ src/pages/SingleArticle.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { fetchArticleById, fetchArticleComments } from "../api";
 
 import LoadingBanner from "../components/LoadingBanner";
-import CommentsList from "../components/CommentsList";
+import CommentCard from "../components/CommentCard.jsx";
 import ArticleRating from "../components/ArticleRating";
 import CommentForm from "../components/CommentForm";
 
@@ -37,37 +37,47 @@ const SingleArticle = ({ topics }) => {
   }
 
   return (
-    <div className="min-h-[calc(100vh-4rem) py-10 max-w-screen-lg w-[95%] m-auto">
-      <section className="article-container">
+    <div className="min-h-[calc(100vh-4rem)] py-10 max-w-screen-lg w-[95%] m-auto">
+      <section className="bg-white p-6">
         {isLoading ? (
           <LoadingBanner typeOfData={"articles"} />
         ) : (
           <>
-            <section className="article-section">
-              <header className="article-header">
-                <h1 className="heading-l">{article.title}</h1>
-                <p className="article-date">Posted on: {timestampToDate(article.created_at)}</p>
-                <p className="article-author">Author: {article.author}</p>
+            <section className="mb-6">
+              <header className="mb-4 border-b pb-4">
+                <h1 className="text-3xl font-bold mb-2">{article.title}</h1>
+                <p className="font-light text-gray-800">
+                  Posted on: {timestampToDate(article.created_at)}
+                </p>
+                <p className="text-gray-500">Author: {article.author}</p>
               </header>
-              <div className="article-img-container">
+              <div className="mb-4">
                 <img
-                  className="article-img"
+                  className="w-full h-auto object-cover rounded-sm"
                   src={article.article_img_url}
                   alt={`${article.topic}`}
                 />
               </div>
-              <div className="article-body">
-                <p>{article.body}</p>
+              <div>
+                <p className="text-lg font-extralight text-justify leading-relaxed">
+                  {article.body}
+                </p>
               </div>
             </section>
-            <section className="article-votes-section">
+            <section className="my-6">
               <ArticleRating articleVotes={article.votes} articleId={article.article_id} />
             </section>
-            <section className="comments-section">
-              <h2>Comments</h2>
-              <div className="comments-container">
+            <section>
+              <h2 className="text-2xl font-semibold mb-4">Comments</h2>
+              <div className="space-y-4">
                 <CommentForm articleId={article_id} setComments={setComments} />
-                <CommentsList comments={comments} setComments={setComments} />
+                {comments?.length === 0 ? (
+                  <p>This article has no comments.</p>
+                ) : (
+                  comments.map((comment, index) => {
+                    return <CommentCard key={index} comment={comment} setComments={setComments} />;
+                  })
+                )}
               </div>
             </section>
           </>
